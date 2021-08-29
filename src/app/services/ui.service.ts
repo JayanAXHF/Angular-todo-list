70af27873d0f8dc00e30c7967368156e0338c0af
Jayan <sunil.chdry@gmail.com>
2021-08-29 18:31:59 +0530

    Final edit

---
+++ src/app/services/ui.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UiService {
+  private showAddTask: boolean = false;
+
+  private subject = new Subject<any>();
+
+  constructor() {}
+
+  toggleAddTask(): void {
+    this.showAddTask = !this.showAddTask;
+    this.subject.next(this.showAddTask);
+    console.log(
+      'file: ui.service.ts ~ line 19 ~ UiService ~ showAddTask',
+      this.showAddTask
+    );
+  }
+
+  onToggle(): Observable<any> {
+    return this.subject.asObservable();
+  }
+}
